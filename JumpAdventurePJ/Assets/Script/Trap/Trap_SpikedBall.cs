cf94193d1614707e42d759a6f3d635686a7b6574
CokeBear99 <desked@naver.com>
2024-09-13 15:31:00 +0900

    [Map Finalize] Level_2 | Level_3
    Fnish Map

--- JumpAdventurePJ/Assets/Script/Trap/Trap_SpikedBall.cs
+++ JumpAdventurePJ/Assets/Script/Trap/Trap_SpikedBall.cs
@@ -1,11 +1,31 @@
+using NUnit.Framework.Constraints;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class Trap_SpikedBall : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private float pushForce;
+    [SerializeField] private float fixForce;
+
     private void Start() 
     {
+        fixForce = Mathf.Abs(pushForce / 2000);
+        Debug.Log(fixForce);
         rb.AddForce( new Vector2(pushForce , 0), ForceMode2D.Impulse);
+    }
+
+    private void FixedUpdate()
+    {
+        if(rb.velocity.x <0)
+        {
+            int direction = -1;
+            rb.AddForce(new Vector2(fixForce * direction,0), ForceMode2D.Impulse);
+        }
+        else
+        {
+            int direction = 1;
+            rb.AddForce(new Vector2(fixForce * direction, 0), ForceMode2D.Impulse);
+        }
     }
 }
