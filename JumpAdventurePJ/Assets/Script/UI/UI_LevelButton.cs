a9535829f5639722ad1fca74260c2a6bc3fd16fb
gyecham <gyecham@naver.com>
2024-09-17 11:02:53 +0900

    [5.UI & System] 9.Save_Game_Info
    Save Game Info

--- JumpAdventurePJ/Assets/Script/UI/UI_LevelButton.cs
+++ JumpAdventurePJ/Assets/Script/UI/UI_LevelButton.cs
@@ -4,22 +4,47 @@ using UnityEngine;
 public class UI_LevelButton : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI levelNumberText;
-    
+    [SerializeField] private TextMeshProUGUI bestTimeText;
+    [SerializeField] private TextMeshProUGUI fruitsText;
+
     private int levelIndex;
 
-    public void SetupButton (int _levelIndex)
+    public void SetupButton(int _levelIndex)
     {
-	    levelIndex = _levelIndex;
-	    levelNumberText.text = "Level " + levelIndex;
+        levelIndex = _levelIndex;
+        levelNumberText.text = "Level " + levelIndex;
+        bestTimeText.text = SetupTimerText();
+        fruitsText.text = SetupFruitsText();
+
     }
+
+    // TimerText 설정
+
+    private string SetupTimerText()
+    {
+        float timerValue = PlayerPrefs.GetFloat("Level_" + levelIndex + "BestTime", 999);
+        Debug.Log(timerValue); // 정상적으로 출력됨
+        return "Best Time : " + timerValue.ToString("00") + "s";
+    }
+
+
+    // FruitsText 설정
+
+    private string SetupFruitsText()
+    {
+        int totalFruits = PlayerPrefs.GetInt("Level_" + levelIndex + "TotalFruits", 0);
+        int fruitsCollected = PlayerPrefs.GetInt("Level_" + levelIndex + "FruitsCollected");
+        return "Fruits : " + fruitsCollected + "/" + totalFruits;
+    }
+
 
     void Start()
     {
-        
+
     }
-    
+
     void Update()
     {
-        
+
     }
 }
